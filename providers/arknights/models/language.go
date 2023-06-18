f83ef40fac7991e55b3c4b03f0af82518a623a18
Denis Chernov <cat.aka.xakep@gmail.com>
2023-06-18 23:12:48 +0300

    Added Arknights provider

--- providers/arknights/models/language.go
+++ providers/arknights/models/language.go
@@ -3,18 +3,22 @@ package models
 type Language string
 
 const (
-	LanguageEn = Language("en")
-	LanguageJp = Language("ja")
+	LanguageUnknown = Language("")
+	LanguageEn      = Language("en")
+	LanguageJp      = Language("ja")
 )
 
 func (l Language) String() string {
 	return string(l)
 }
 
-func ParseFormat(value string) Language {
-	if value == string(LanguageJp) {
+func ParseLanguage(value string) Language {
+	switch value {
+	case LanguageEn.String():
+		return LanguageEn
+	case LanguageJp.String():
 		return LanguageJp
+	default:
+		return LanguageUnknown
 	}
-
-	return LanguageEn
 }
