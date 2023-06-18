877f10282240b5fe9bd4d753b260011416f8fd03
Denis Chernov <cat.aka.xakep@gmail.com>
2023-06-18 17:47:30 +0300

    Added BleepingComputer site meta parsing

--- providers/bleepingcomputer/mappers/news_feed.go
+++ providers/bleepingcomputer/mappers/news_feed.go
@@ -33,6 +33,20 @@ func PluckItems(contents *xmlquery.Node) []*models.Entry {
 	return entries
 }
 
+func PickSiteMeta(contents *xmlquery.Node) *models.Feed {
+	channel := xmlquery.FindOne(contents, "//channel")
+	if channel == nil {
+		return nil
+	}
+
+	return &models.Feed{
+		Title:       parseText(channel.SelectElement("title")),
+		Description: parseText(channel.SelectElement("description")),
+		Language:    parseText(channel.SelectElement("language")),
+		Published:   parsePublished(channel.SelectElement("pubDate")),
+	}
+}
+
 func parseText(value *xmlquery.Node) string {
 	if value == nil {
 		return ""
@@ -48,9 +62,14 @@ func parsePublished(value *xmlquery.Node) time.Time {
 	}
 
 	timestamp, err := time.Parse(time.RFC1123Z, text)
-	if err != nil {
-		return constants.TimeZero
+	if err == nil {
+		return timestamp
 	}
 
-	return timestamp
+	timestamp, err = time.Parse(time.RFC1123, text)
+	if err == nil {
+		return timestamp
+	}
+
+	return constants.TimeZero
 }
--- providers/bleepingcomputer/mappers/news_feed_test.go
+++ providers/bleepingcomputer/mappers/news_feed_test.go
@@ -2,9 +2,9 @@ package mappers_test
 
 import (
 	"bytes"
-	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/antchfx/xmlquery"
 	"github.com/imcrazytwkr/feedhub/models"
@@ -32,8 +32,6 @@ func TestNewsItemsExtraction(t *testing.T) {
 		t.Fatal("No entries were parsed")
 	}
 
-	fmt.Printf("%v", entries)
-
 	var expectedEntries []*models.Entry
 	err = testutil.ReadJson("test_data/expected_entries.json", &expectedEntries)
 	if err != nil {
@@ -43,5 +41,46 @@ func TestNewsItemsExtraction(t *testing.T) {
 	if !testutil.SlicesEqual(entries, expectedEntries) {
 		t.Log("slices aren't equal")
 		t.FailNow()
+	}
+}
+
+func TestSiteMetaExtraction(t *testing.T) {
+	feedFile, err := os.ReadFile("test_data/news_feed.xml")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	contents, err := xmlquery.Parse(bytes.NewReader(feedFile))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if contents == nil {
+		t.Fatal("rss feed is NIL!")
+	}
+
+	actualMeta := mappers.PickSiteMeta(contents)
+
+	var expectedMeta models.Feed
+	testutil.ReadJson("test_data/expected_meta.json", &expectedMeta)
+
+	if actualMeta.Title != expectedMeta.Title {
+		t.Logf("Titles mismatch, expected %q, got %q", expectedMeta.Title, actualMeta.Title)
+		t.Fail()
+	}
+
+	if actualMeta.Description != expectedMeta.Description {
+		t.Logf("Descriptions mismatch, expected %q, got %q", expectedMeta.Description, actualMeta.Description)
+		t.Fail()
+	}
+
+	if actualMeta.Language != expectedMeta.Language {
+		t.Logf("Language mismatch, expected %q, got %q", expectedMeta.Language, actualMeta.Language)
+		t.Fail()
+	}
+
+	if !actualMeta.Published.Equal(expectedMeta.Published) {
+		t.Logf("Published mismatch, expected %q, got %q", expectedMeta.Published.Format(time.RFC3339), actualMeta.Published.Format(time.RFC3339))
+		t.Fail()
 	}
 }
