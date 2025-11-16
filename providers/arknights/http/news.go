1a16febf3647521be5056da6f27f679b9c5150cf
Denis Chernov <cat.aka.xakep@gmail.com>
2025-11-16 14:11:36 +0200

    Added error handling and logging when language requested does not have a mapping (just in case)

--- providers/arknights/http/news.go
+++ providers/arknights/http/news.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -15,7 +16,13 @@ import (
 func (c *ArknightsClient) GetNews(ctx context.Context, language m.Language) ([]byte, error) {
 	log := zerolog.Ctx(ctx)
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, getNewsUrl(language), nil)
+	uri, err := getNewsUrl(language)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to generate request URI")
+		return nil, models.NewHttpError(http.StatusInternalServerError, nil)
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, uri, nil)
 	if err != nil {
 		log.Error().Err(err).Msg("failed to create request to fetch news feed")
 		return nil, models.NewHttpError(http.StatusInternalServerError, nil)
@@ -24,13 +31,21 @@ func (c *ArknightsClient) GetNews(ctx context.Context, language m.Language) ([]b
 	return httputil.FetchRequest(c.httpClient, req)
 }
 
-func getNewsUrl(language m.Language) string {
-	uri, _ := url.Parse(hostPrefixes[language])
+func getNewsUrl(language m.Language) (string, error) {
+	hostPrefix, exists := hostPrefixes[language]
+	if !exists {
+		return "", fmt.Errorf("mapping for language %s does not exist", language)
+	}
+
+	uri, err := url.Parse(hostPrefix)
+	if err != nil {
+		return "", err
+	}
 
 	uri.Path = newsPath
 	uri.RawQuery = getNewsQuery(language)
 
-	return uri.String()
+	return uri.String(), nil
 }
 
 func getNewsQuery(language m.Language) string {
