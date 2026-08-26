57511732dc424cd75a53bd35ac61b79f275a7a86
Denis Chernov <d3lusion4l.nom4d@gmail.com>
2026-08-26 16:58:49 +0300

    Extracted localised slug route helper

--- routes/aws/blogs.go
+++ routes/aws/blogs.go
@@ -5,12 +5,12 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/imcrazytwkr/feedhub/models"
+	"github.com/imcrazytwkr/feedhub/routes/common"
 	"github.com/imcrazytwkr/feedhub/utils/ginutil"
 )
 
 func (r *AWSRouter) handleBlogs(c *gin.Context) {
-	category, lang := resolveBlogsPath(
+	category, lang := common.ParseSlugWithLocale(
 		strings.TrimRight(c.Param("slug"), "/"),
 		strings.TrimRight(c.Param("lang"), "/"),
 	)
@@ -27,35 +27,4 @@ func (r *AWSRouter) handleBlogs(c *gin.Context) {
 	}
 
 	ginutil.RenderFeed(c, feed)
-}
-
-// These cases are excessively documented because nested optional params are
-// terrible from code standpoint but good for UX
-func resolveBlogsPath(category string, lang string) (string, models.Language) {
-	// `//:lang` is normalized to `/:lang` by GIN so it is impossible for "lang"
-	// to be set when "category" is empty.
-	if len(category) == 0 {
-		// No parameters (all categories in English)
-		return "", models.LanguageEn
-	}
-
-	if len(lang) == 0 {
-		language := models.ParseLanguage(category)
-
-		// `/aws/:lang` (all posts in language)
-		if language != models.LanguageUnknown {
-			return "", language
-		}
-
-		// `/aws/:category` (posts for category in English)
-		return category, models.LanguageEn
-	}
-
-	// `/aws/:category/:language` (posts for category in language)
-	language := models.ParseLanguage(lang)
-	if language == models.LanguageUnknown {
-		return category, models.LanguageEn
-	}
-
-	return category, language
 }
